d322a034ba02405121f0e96804b9115dc683447c
any-ova <annfragen@gmail.com>
2024-05-26 14:31:55 +0300

    Разделение на файлы

---
+++ learning/any.h
@@ -0,0 +1,12 @@
+#ifndef ANY_H
+#define ANY_H
+#include <gtk/gtk.h>
+
+void on_back_button_clicked(GtkWidget *widget, gpointer data);
+void change_img(GtkWidget *widget);
+void slide(GtkWidget *widget);
+void yes_btn_click(GtkWidget *widget, gpointer data);
+void no_btn_click(GtkWidget *widget, gpointer data);
+void f(int argc, char *argv[]);
+
+#endif
