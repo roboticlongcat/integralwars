bed77aa836429802ab9d609808bf3ae945c293d3
thePontific <150601112+thePontific@users.noreply.github.com>
2024-06-02 20:02:43 +0300

    Update main.cpp

--- game/main.cpp
+++ game/main.cpp
@@ -1,63 +1,15 @@
-#include <gtk/gtk.h>
-#include <vector>
-#include <utility> // для std::pair
-#include <cstdlib> // для rand и srand
-#include <ctime>   // для time
-#include <string>
-#include <algorithm>
-#include <iostream>
+#include "main.h"
 using namespace std;
 
-// Структура для представления изображений вопросов и ответов
-struct ImagePair {
-    std::string question; // Имя файла изображения вопроса
-    std::string answer;   // Имя файла изображения ответа
-};
-
-// Глобальный вектор изображений вопросов и ответов
-std::vector<ImagePair> images = {
-    {"1.png", "ans1.png"},
-    {"2.png", "ans2.png"},
-    {"3.png", "ans3.png"},
-    {"4.png", "ans4.png"},
-    {"5.png", "ans5.png"},
-    {"6.png", "ans6.png"},
-    {"7.png", "ans7.png"},
-    {"8.png", "ans8.png"},
-    {"9.png", "ans9.png"},
-    {"10.png", "ans10.png"},
-    {"11.png", "ans11.png"},
-    {"12.png", "ans12.png"},
-    {"13.png", "ans13.png"},
-    {"14.png", "ans14.png"},
-    {"15.png", "ans15.png"},
-    {"16.png", "ans16.png"},
-    {"17.png", "ans17.png"},
-    {"18.png", "ans18.png"},
-    {"19.png", "ans19.png"},
-    {"20.png", "ans20.png"}
-};
-
-std::string questionImageFile;
-std::vector<std::string> currentAnswerCards;
-int statistic_point=0;
-int lvl_point=0;
-    GtkTextBuffer *buffer;
-    GtkWidget *view;
-bool firstTry=true;
-const int BUTTON_WIDTH = 750;
-const int BUTTON_HEIGHT = 200;
-
-
-// Прототип функции button_clicked
+// РџСЂРѕС‚РѕС‚РёРї С„СѓРЅРєС†РёРё button_clicked
 static void button_clicked(GtkWidget* button, gpointer data);
 
-// Функция для перемешивания изображений
+// Р¤СѓРЅРєС†РёСЏ РґР»СЏ РїРµСЂРµРјРµС€РёРІР°РЅРёСЏ РёР·РѕР±СЂР°Р¶РµРЅРёР№
 void shuffleImages() {
     std::random_shuffle(images.begin(), images.end());
 }
 
-// Функция для получения случайного изображения вопроса
+// Р¤СѓРЅРєС†РёСЏ РґР»СЏ РїРѕР»СѓС‡РµРЅРёСЏ СЃР»СѓС‡Р°Р№РЅРѕРіРѕ РёР·РѕР±СЂР°Р¶РµРЅРёСЏ РІРѕРїСЂРѕСЃР°
 std::string getRandomQuestionImage() {
     int index = rand() % images.size();
     return images[index].question;
@@ -66,7 +18,7 @@ std::string getRandomQuestionImage() {
 std::string getRandomAnswerImage(const std::string& questionImage) {
     std::vector<std::string> availableAnswerImages;
 
-    // Добавляем все доступные ответы, кроме ответа на переданный вопрос
+    // Р”РѕР±Р°РІР»СЏРµРј РІСЃРµ РґРѕСЃС‚СѓРїРЅС‹Рµ РѕС‚РІРµС‚С‹, РєСЂРѕРјРµ РѕС‚РІРµС‚Р° РЅР° РїРµСЂРµРґР°РЅРЅС‹Р№ РІРѕРїСЂРѕСЃ
     for (const auto& pair : images) {
         string vopros = "image/" + pair.question;
         if (vopros != questionImage) {
@@ -74,29 +26,29 @@ std::string getRandomAnswerImage(const std::string& questionImage) {
         }
     }
 
-    // Выбираем случайный индекс из доступных ответов
+    // Р’С‹Р±РёСЂР°РµРј СЃР»СѓС‡Р°Р№РЅС‹Р№ РёРЅРґРµРєСЃ РёР· РґРѕСЃС‚СѓРїРЅС‹С… РѕС‚РІРµС‚РѕРІ
     int index = rand() % availableAnswerImages.size();
     return availableAnswerImages[index];
 }
 
-// Функция для создания массива карточек с ответами
+// Р¤СѓРЅРєС†РёСЏ РґР»СЏ СЃРѕР·РґР°РЅРёСЏ РјР°СЃСЃРёРІР° РєР°СЂС‚РѕС‡РµРє СЃ РѕС‚РІРµС‚Р°РјРё
 std::vector<std::string> createAnswerCards(const std::string& questionImage) {
-    // Создаем вектор для хранения имен файлов изображений ответов
+    // РЎРѕР·РґР°РµРј РІРµРєС‚РѕСЂ РґР»СЏ С…СЂР°РЅРµРЅРёСЏ РёРјРµРЅ С„Р°Р№Р»РѕРІ РёР·РѕР±СЂР°Р¶РµРЅРёР№ РѕС‚РІРµС‚РѕРІ
     std::vector<std::string> answerImages;
 
-    // Добавляем четыре уникальных случайных ответа
+    // Р”РѕР±Р°РІР»СЏРµРј С‡РµС‚С‹СЂРµ СѓРЅРёРєР°Р»СЊРЅС‹С… СЃР»СѓС‡Р°Р№РЅС‹С… РѕС‚РІРµС‚Р°
     while (answerImages.size() < 4) {
         std::string randomAnswer = getRandomAnswerImage(questionImage);
-        // Проверяем уникальность выбранной картинки
+        // РџСЂРѕРІРµСЂСЏРµРј СѓРЅРёРєР°Р»СЊРЅРѕСЃС‚СЊ РІС‹Р±СЂР°РЅРЅРѕР№ РєР°СЂС‚РёРЅРєРё
         if (std::find(answerImages.begin(), answerImages.end(), randomAnswer) == answerImages.end()) {
             answerImages.push_back(randomAnswer);
         }
     }
 
-    // Выбираем случайный индекс для замены
+    // Р’С‹Р±РёСЂР°РµРј СЃР»СѓС‡Р°Р№РЅС‹Р№ РёРЅРґРµРєСЃ РґР»СЏ Р·Р°РјРµРЅС‹
     int indexToReplace = rand() % 4;
 
-    // Заменяем случайный элемент на правильный ответ
+    // Р—Р°РјРµРЅСЏРµРј СЃР»СѓС‡Р°Р№РЅС‹Р№ СЌР»РµРјРµРЅС‚ РЅР° РїСЂР°РІРёР»СЊРЅС‹Р№ РѕС‚РІРµС‚
     for (const auto& pair : images) {
         string vopros = "image/" + pair.question;
         if (vopros == questionImage) {
@@ -108,9 +60,9 @@ std::vector<std::string> createAnswerCards(const std::string& questionImage) {
     return answerImages;
 }
 
-// Функция для очистки предыдущих элементов в сетке
+// Р¤СѓРЅРєС†РёСЏ РґР»СЏ РѕС‡РёСЃС‚РєРё РїСЂРµРґС‹РґСѓС‰РёС… СЌР»РµРјРµРЅС‚РѕРІ РІ СЃРµС‚РєРµ
 void clearGrid(GtkWidget *grid) {
-    // Удаляем все дочерние элементы из сетки
+    // РЈРґР°Р»СЏРµРј РІСЃРµ РґРѕС‡РµСЂРЅРёРµ СЌР»РµРјРµРЅС‚С‹ РёР· СЃРµС‚РєРё
     GList *children, *iter;
     children = gtk_container_get_children(GTK_CONTAINER(grid));
     for (iter = children; iter != NULL; iter = g_list_next(iter)) {
@@ -120,54 +72,54 @@ void clearGrid(GtkWidget *grid) {
 }
 
 void startNewLevel(GtkWidget *grid) {
-    // Очищаем предыдущие элементы в сетке
+    // РћС‡РёС‰Р°РµРј РїСЂРµРґС‹РґСѓС‰РёРµ СЌР»РµРјРµРЅС‚С‹ РІ СЃРµС‚РєРµ
     clearGrid(grid);
 
-    // Получаем случайное изображение вопроса
+    // РџРѕР»СѓС‡Р°РµРј СЃР»СѓС‡Р°Р№РЅРѕРµ РёР·РѕР±СЂР°Р¶РµРЅРёРµ РІРѕРїСЂРѕСЃР°
     questionImageFile = "image/" + getRandomQuestionImage();
-    std::cout << "Имя изображения вопроса: " << questionImageFile << std::endl;
+    std::cout << "РРјСЏ РёР·РѕР±СЂР°Р¶РµРЅРёСЏ РІРѕРїСЂРѕСЃР°: " << questionImageFile << std::endl;
 
-    // Получаем массив карточек ответов
+    // РџРѕР»СѓС‡Р°РµРј РјР°СЃСЃРёРІ РєР°СЂС‚РѕС‡РµРє РѕС‚РІРµС‚РѕРІ
     std::vector<std::string> answerCards = createAnswerCards(questionImageFile);
     currentAnswerCards = answerCards;
 
-    // Создаем изображение вопроса и добавляем его в сетку
+    // РЎРѕР·РґР°РµРј РёР·РѕР±СЂР°Р¶РµРЅРёРµ РІРѕРїСЂРѕСЃР° Рё РґРѕР±Р°РІР»СЏРµРј РµРіРѕ РІ СЃРµС‚РєСѓ
     GtkWidget *questionImage = gtk_image_new();
     gtk_image_set_from_file(GTK_IMAGE(questionImage), questionImageFile.c_str());
     gtk_grid_attach(GTK_GRID(grid), questionImage, 0, 0, 3, 1);
     gtk_widget_set_halign(questionImage, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(questionImage, GTK_ALIGN_CENTER);
 
-    // Создаем кнопки с изображениями ответов и добавляем их в сетку
+    // РЎРѕР·РґР°РµРј РєРЅРѕРїРєРё СЃ РёР·РѕР±СЂР°Р¶РµРЅРёСЏРјРё РѕС‚РІРµС‚РѕРІ Рё РґРѕР±Р°РІР»СЏРµРј РёС… РІ СЃРµС‚РєСѓ
     for (int i = 0; i < 4; ++i) {
-        // Получаем текущее изображение ответа
+        // РџРѕР»СѓС‡Р°РµРј С‚РµРєСѓС‰РµРµ РёР·РѕР±СЂР°Р¶РµРЅРёРµ РѕС‚РІРµС‚Р°
         std::string currImage = "image/" + answerCards[i];
-        std::cout << "Имя изображения ответа " << i + 1 << ": " << currImage << std::endl;
+        std::cout << "РРјСЏ РёР·РѕР±СЂР°Р¶РµРЅРёСЏ РѕС‚РІРµС‚Р° " << i + 1 << ": " << currImage << std::endl;
 
-        // Создаем кнопку и устанавливаем изображение
+        // РЎРѕР·РґР°РµРј РєРЅРѕРїРєСѓ Рё СѓСЃС‚Р°РЅР°РІР»РёРІР°РµРј РёР·РѕР±СЂР°Р¶РµРЅРёРµ
         GtkWidget *button = gtk_button_new();
         GtkWidget *image = gtk_image_new_from_file(currImage.c_str());
         if (image == NULL) {
-            g_warning("Не удалось загрузить изображение");
+            g_warning("РќРµ СѓРґР°Р»РѕСЃСЊ Р·Р°РіСЂСѓР·РёС‚СЊ РёР·РѕР±СЂР°Р¶РµРЅРёРµ");
         } else {
             gtk_button_set_image(GTK_BUTTON(button), image);
             gtk_widget_set_halign(image, GTK_ALIGN_CENTER);
             gtk_widget_set_valign(image, GTK_ALIGN_CENTER);
             gtk_widget_set_size_request(image, -1, -1);
         }
-        // Устанавливаем связанное с кнопкой изображение
+        // РЈСЃС‚Р°РЅР°РІР»РёРІР°РµРј СЃРІСЏР·Р°РЅРЅРѕРµ СЃ РєРЅРѕРїРєРѕР№ РёР·РѕР±СЂР°Р¶РµРЅРёРµ
         g_object_set_data(G_OBJECT(button), "image_file", g_strdup(currImage.c_str()));
 
-        // Рассчитываем позицию кнопки в сетке и добавляем ее
+        // Р Р°СЃСЃС‡РёС‚С‹РІР°РµРј РїРѕР·РёС†РёСЋ РєРЅРѕРїРєРё РІ СЃРµС‚РєРµ Рё РґРѕР±Р°РІР»СЏРµРј РµРµ
         gtk_grid_attach(GTK_GRID(grid), button, i % 2, i / 2 + 1, 1, 1);
         gtk_widget_set_size_request(button, BUTTON_WIDTH, BUTTON_HEIGHT);
 
 
-        // Устанавливаем обработчик события клика на кнопку
+        // РЈСЃС‚Р°РЅР°РІР»РёРІР°РµРј РѕР±СЂР°Р±РѕС‚С‡РёРє СЃРѕР±С‹С‚РёСЏ РєР»РёРєР° РЅР° РєРЅРѕРїРєСѓ
         g_signal_connect(button, "clicked", G_CALLBACK(button_clicked), grid);
     }
 
-    // Создаем текст и добавляем его в сетку
+    // РЎРѕР·РґР°РµРј С‚РµРєСЃС‚ Рё РґРѕР±Р°РІР»СЏРµРј РµРіРѕ РІ СЃРµС‚РєСѓ
 std::string text = "Number of correct answers on the first attempt " + std::to_string(statistic_point) + "/" + std::to_string(lvl_point);
 GtkWidget *label = gtk_label_new(text.c_str());
 
@@ -175,20 +127,20 @@ GtkWidget *label = gtk_label_new(text.c_str());
     gtk_widget_set_halign(label, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(label, GTK_ALIGN_CENTER);
 
-    // Показываем все элементы сетки
+    // РџРѕРєР°Р·С‹РІР°РµРј РІСЃРµ СЌР»РµРјРµРЅС‚С‹ СЃРµС‚РєРё
     gtk_widget_show_all(grid);
         lvl_point += 1;
 }
 
 
 
-// Обработчик события клика на кнопке
+// РћР±СЂР°Р±РѕС‚С‡РёРє СЃРѕР±С‹С‚РёСЏ РєР»РёРєР° РЅР° РєРЅРѕРїРєРµ
 static void button_clicked(GtkWidget* button, gpointer data) {
-    // Получаем имя файла изображения, связанное с кнопкой
+    // РџРѕР»СѓС‡Р°РµРј РёРјСЏ С„Р°Р№Р»Р° РёР·РѕР±СЂР°Р¶РµРЅРёСЏ, СЃРІСЏР·Р°РЅРЅРѕРµ СЃ РєРЅРѕРїРєРѕР№
     const gchar *imageFile = (const gchar *)g_object_get_data(G_OBJECT(button), "image_file");
-    cout << "Имя файла изображения, связанное с кнопкой: " << imageFile << endl;
+    cout << "РРјСЏ С„Р°Р№Р»Р° РёР·РѕР±СЂР°Р¶РµРЅРёСЏ, СЃРІСЏР·Р°РЅРЅРѕРµ СЃ РєРЅРѕРїРєРѕР№: " << imageFile << endl;
 
-    // Проверяем правильность ответа
+    // РџСЂРѕРІРµСЂСЏРµРј РїСЂР°РІРёР»СЊРЅРѕСЃС‚СЊ РѕС‚РІРµС‚Р°
     bool correctAnswer = false;
     cout<<firstTry<<" "<<statistic_point<<endl;
     for (const auto& pair : images) {
@@ -196,7 +148,7 @@ static void button_clicked(GtkWidget* button, gpointer data) {
         if (vopros == questionImageFile) {
             string otvet = "image/" + pair.answer;
             if (otvet == imageFile) {
-                cout << "Правильный ответ!" << endl;
+                cout << "РџСЂР°РІРёР»СЊРЅС‹Р№ РѕС‚РІРµС‚!" << endl;
                 cout<<firstTry<<" "<<statistic_point<<endl;
                 correctAnswer = true;
                 if (firstTry==true){
@@ -205,7 +157,7 @@ static void button_clicked(GtkWidget* button, gpointer data) {
                  firstTry=true;
             }
             else {
-                cout << "Неправильный ответ!" << endl;
+                cout << "РќРµРїСЂР°РІРёР»СЊРЅС‹Р№ РѕС‚РІРµС‚!" << endl;
                 firstTry=false;
                 cout<<firstTry<<" "<<statistic_point<<endl;
             }
@@ -213,13 +165,13 @@ static void button_clicked(GtkWidget* button, gpointer data) {
         }
     }
 
-    // Если ответ правильный, начинаем новый уровень
+    // Р•СЃР»Рё РѕС‚РІРµС‚ РїСЂР°РІРёР»СЊРЅС‹Р№, РЅР°С‡РёРЅР°РµРј РЅРѕРІС‹Р№ СѓСЂРѕРІРµРЅСЊ
     if (correctAnswer) {
         startNewLevel(GTK_WIDGET(data));
     }
 }
 
-// Функция для инициализации GTK и создания окна
+// Р¤СѓРЅРєС†РёСЏ РґР»СЏ РёРЅРёС†РёР°Р»РёР·Р°С†РёРё GTK Рё СЃРѕР·РґР°РЅРёСЏ РѕРєРЅР°
 static gboolean activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
     GtkWidget *grid;
@@ -227,43 +179,43 @@ static gboolean activate(GtkApplication *app, gpointer user_data) {
 
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    srand(time(NULL)); // Инициализация генератора случайных чисел
+    srand(time(NULL)); // РРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РіРµРЅРµСЂР°С‚РѕСЂР° СЃР»СѓС‡Р°Р№РЅС‹С… С‡РёСЃРµР»
 
-    // Установка заголовка окна и его размеров
+    // РЈСЃС‚Р°РЅРѕРІРєР° Р·Р°РіРѕР»РѕРІРєР° РѕРєРЅР° Рё РµРіРѕ СЂР°Р·РјРµСЂРѕРІ
     gtk_window_set_title(GTK_WINDOW(window), "Window");
     gtk_window_set_default_size(GTK_WINDOW(window), 1000, 800);
 
-    // Создание сетки для размещения элементов
+    // РЎРѕР·РґР°РЅРёРµ СЃРµС‚РєРё РґР»СЏ СЂР°Р·РјРµС‰РµРЅРёСЏ СЌР»РµРјРµРЅС‚РѕРІ
     grid = gtk_grid_new();
     gtk_container_add(GTK_CONTAINER(window), grid);
 
-    // Запускаем первый уровень
+    // Р—Р°РїСѓСЃРєР°РµРј РїРµСЂРІС‹Р№ СѓСЂРѕРІРµРЅСЊ
     startNewLevel(grid);
 
     gtk_widget_show_all(window);
 
-    // Запуск главного цикла обработки событий GTK
+    // Р—Р°РїСѓСЃРє РіР»Р°РІРЅРѕРіРѕ С†РёРєР»Р° РѕР±СЂР°Р±РѕС‚РєРё СЃРѕР±С‹С‚РёР№ GTK
     gtk_main();
 
     return TRUE;
 }
 
-// Точка входа в приложение
+// РўРѕС‡РєР° РІС…РѕРґР° РІ РїСЂРёР»РѕР¶РµРЅРёРµ
 int main(int argc, char **argv) {
-    // Создание приложения GTK
+    // РЎРѕР·РґР°РЅРёРµ РїСЂРёР»РѕР¶РµРЅРёСЏ GTK
     GtkApplication *app;
     int status;
 
-    // Создание нового приложения
+    // РЎРѕР·РґР°РЅРёРµ РЅРѕРІРѕРіРѕ РїСЂРёР»РѕР¶РµРЅРёСЏ
     app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
 
-    // Подключение обработчика событий активации приложения
+    // РџРѕРґРєР»СЋС‡РµРЅРёРµ РѕР±СЂР°Р±РѕС‚С‡РёРєР° СЃРѕР±С‹С‚РёР№ Р°РєС‚РёРІР°С†РёРё РїСЂРёР»РѕР¶РµРЅРёСЏ
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 
-    // Запуск приложения
+    // Р—Р°РїСѓСЃРє РїСЂРёР»РѕР¶РµРЅРёСЏ
     status = g_application_run(G_APPLICATION(app), argc, argv);
 
-    // Освобождение ресурсов приложения
+    // РћСЃРІРѕР±РѕР¶РґРµРЅРёРµ СЂРµСЃСѓСЂСЃРѕРІ РїСЂРёР»РѕР¶РµРЅРёСЏ
     g_object_unref(app);
 
     return status;
